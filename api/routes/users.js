ff09d3e0f4cb7d6326af0187f12e75e7f99af1b2
blessberry <beraka.emmanuel@protonmail.ch>
2020-10-13 15:29:03 +0000

    Add xml response type to GET /users route

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const users = require('../models/users.json');
+const builder = require('xmlbuilder');
 
 const router = express.Router();
 
-const getUsers = (req, res) =>
-    res.send(users);
+const getUsers = (req, res) => { 
+    if (req.accepts('application/xml')) 
+        res.type('application/xml').send(builder.create(users).end({ pretty: true}));
+    else
+        res.send(users);
+};
 const getUser = (req, res) => 
     res.send(users.find(user => user.id===req.params.id));
 
